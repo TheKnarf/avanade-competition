5b7e01b831f12a472cb679498262322d0e586c62
hkwaller <hkwaller@gmail.com>
2014-11-03 13:24:39 +0100

    mapfixes

--- README.md
+++ README.md
@@ -1,5 +1,2 @@
 avanade-competition
 ===================
-
-
-API key for google maps: AIzaSyAflzKajHyhhRO4Bt8kmr99h2egCVlo3LM
--- js/app.js
+++ js/app.js
@@ -5,5 +5,17 @@ var app = angular.module('app', [
 
 
 .controller("HomeCtrl", function ($scope) {    
-    
+    $scope.options = {
+        map: {
+          center: new google.maps.LatLng(59.913869, 10.752245),
+          zoom: 10,
+          mapTypeId: google.maps.MapTypeId.TERRAIN
+        },
+        notselected: {
+          icon: 'https://maps.gstatic.com/mapfiles/ms2/micons/red-dot.png',
+        },
+        selected: {
+          icon: 'https://maps.gstatic.com/mapfiles/ms2/micons/yellow-dot.png',
+        }
+      };
 });
