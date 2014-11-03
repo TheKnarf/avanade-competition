160e6ae179b4ce88b9112d37af14d3ac2eae0f58
hkwaller <hkwaller@gmail.com>
2014-11-03 14:51:56 +0100

    push

--- js/app.js
+++ js/app.js
@@ -3,7 +3,6 @@ var app = angular.module('app', [
     'AngularGM' 
 ])
 
-
 .controller("HomeCtrl", function ($scope, $http) {    
     $scope.options = {
         map: {
@@ -17,22 +16,46 @@ var app = angular.module('app', [
           icon: 'https://maps.gstatic.com/mapfiles/ms2/micons/yellow-dot.png',
         }
       };
-    
-    
-    $scope.assignments = [];
-    
+
+    $scope.results= [];
     $scope.getAssignments = function() {
         $http.get('novatransport.php?endpoint=assignments').
             success(function(data, status, headers, config) {
                 $scope.assignments = data;
-                console.log($scope.assignments);
+                angular.forEach(data, function(val, key) {
+                    console.log(val);
+                    var obj = {'id':val.id, 'lat': val.startLatitude, 'long': val.startLongitude};
+                    $scope.results.push(obj);
+                    console.log(obj);
+                });
+                console.log($scope.results);
                 $scope.center = new google.maps.LatLng(data[0].endLatitude, data[0].endLongitude);
-                $scope.$broadcast('gmMarkersUpdate');
+                $scope.$broadcast('gmMarkersUpdate', 'assignments');
             }).
             error(function(data, status, headers, config) {
                 console.log('error');
           });
         
+    }
+    
+    $scope.getVehicleLocations = function() {
+        $http.get('novatransport.php?endpoint=vehiclelocations/1').
+            success(function(data, status, headers, config) {
+                $scope.vehiclelocations = data;
+                $scope.vehiclelocations.splice(20, 0);
+                console.log($scope.vehiclelocations);
+                $scope.$broadcast('gmMarkersUpdate', 'vehiclelocations');
+            }).
+            error(function(data, status, headers, config) {
+                console.log('error');
+          });
+    }
+    $scope.getAssignmentOptions = function(object) {
+        return angular.extend(
+         { title: object.notes },
+         object.selected ? $scope.options.selected :
+            $scope.options.notselected
+        );
     }
     
     $scope.$on('gmMarkersUpdated', function(event, objects) {
